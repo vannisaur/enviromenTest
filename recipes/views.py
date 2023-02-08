c58e06fff748a693d3f232c3d0271c898b7622da
Vanni <bielvanni@hotmail.com>
2023-02-08 10:38:13 -0300

    Pastas Head e Partials HTML

--- recipes/views.py
+++ recipes/views.py
@@ -5,6 +5,6 @@ from django.shortcuts import render
 
 
 def view_home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'nome': 'Gabriel Vanni',
         'idade': 23})
