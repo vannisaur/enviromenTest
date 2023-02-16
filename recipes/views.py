96cc2b39bb8838b17681f68cfe2353458081aba0
Gabriel Vanni - Dev Shiloh <devs.shiloh@gmail.com>
2023-02-16 17:45:00 -0300

    Inserindo dados no HTML

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,18 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 # Create your views here.
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'nome': 'Gabriel Vanni',
-        'idade': 23})
+        'recipes': [make_recipe() for _ in range(12)],
+    })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'nome': 'Gabriel Vanni',
-        'idade': 23})
+        'recipe': make_recipe(),
+    })
